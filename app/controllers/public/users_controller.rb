d62673afd079fe6297971366b88f18d79f6c18b4
EC2 Default User <ec2-user@ip-172-31-10-205.ap-northeast-1.compute.internal>
2023-05-26 13:50:09 +0000

    Update

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,7 @@
 class Public::UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
+    @posts = @user.posts.page(params[:page]).per(10)
     @tag_lists = Tag.all
   end
 
