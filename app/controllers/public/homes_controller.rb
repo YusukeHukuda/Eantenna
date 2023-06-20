06ba2b5501edcfd09910cc367081b87f827e1675
EC2 Default User <ec2-user@ip-172-31-10-205.ap-northeast-1.compute.internal>
2023-06-20 08:35:08 +0000

    Update

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
     @tag_lists = Tag.all
-  end
-
-  def about
+    @random_post = Post.order("RANDOM()").first
+    @header_text = "E.antenna"
+    @header_text_sub = "E.antennaへようこそ。このサイトはあなたの身近にあるemotionalな風景を共有できるサイトです。"
   end
 end
